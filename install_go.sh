3be9275f04c71975fbd1cf0ec9838878a4a877d3
Tim1207 <Tim1207@localhost>
2024-07-01 00:24:49 -0500

    update go version and setDPDK.sh

--- install_go.sh
+++ install_go.sh
@@ -4,8 +4,8 @@ sudo rm -rf ~/go
 
 mkdir -p ~/go/{bin,pkg,src}
 
-wget https://dl.google.com/go/go1.19.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -zxf go1.19.3.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.21.8.linux-amd64.tar.gz
+sudo tar -C /usr/local -zxf go1.21.8.linux-amd64.tar.gz
 
 # The following assume that your shell is bash
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc
@@ -14,4 +14,4 @@ echo 'export PATH=$PATH:$GOPATH/bin:$GOROOT/bin' >> ~/.bashrc
 echo 'export GO111MODULE=auto' >> ~/.bashrc
 source ~/.bashrc
 
-rm go1.19.3.linux-amd64.tar.gz
+rm go1.21.8.linux-amd64.tar.gz
